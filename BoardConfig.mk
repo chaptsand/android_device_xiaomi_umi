77ebf6f9851b00bca6edaa3cf63f1ba734a13b66
Peng Xu <pxu@qca.qualcomm.com>
2017-02-07 15:16:52 -0800

    umi: Enable support for IEEE80211AX
     * This is to allow supporting WiFi 6 mobile hotspots,
       to be noted this is only supported on SDM855+ platforms.
    
    Change-Id: Iab61bd12499968e8823b3078acd1c7b8bce46136

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -212,6 +212,7 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 CONFIG_ACS := true
 CONFIG_IEEE80211AC := true
+CONFIG_IEEE80211AX := true
 
 # Inherit from the proprietary version
 include vendor/xiaomi/umi/BoardConfigVendor.mk
