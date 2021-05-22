06b7b33ee56011779b549a4d48335409fdc8d3d4
Bruno Martins <bgcngm@gmail.com>
2021-05-22 14:52:37 +0100

    umi: libqti-perfd-client: Remove namespace declaration
    It's pointless when using extern "C".
    
    Change-Id: Ibdf9f06a70aa3a75687b33781c78cf2172bb334d

--- libqti-perfd-client/client.cpp
+++ libqti-perfd-client/client.cpp
@@ -1,10 +1,8 @@
 #include <stdint.h>
 
-namespace android {
-    extern "C" void perf_get_feedback() {}
-    extern "C" void perf_hint() {}
-    extern "C" void perf_lock_acq() {}
-    extern "C" void perf_lock_cmd() {}
-    extern "C" void perf_lock_rel() {}
-    extern "C" void perf_lock_use_profile() {}
-}
+extern "C" void perf_get_feedback() {}
+extern "C" void perf_hint() {}
+extern "C" void perf_lock_acq() {}
+extern "C" void perf_lock_cmd() {}
+extern "C" void perf_lock_rel() {}
+extern "C" void perf_lock_use_profile() {}
