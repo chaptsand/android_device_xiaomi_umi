3bf2d8264c4a66165d9daec63d997b12a516b553
chaptsand <chaptsand@gmail.com>
2021-08-19 22:52:30 +0800

    umi: Import audio_policy_configuration.xml from LA.UM.9.12.r1-12500-SMxx50.0
    Change-Id: I6327643a9cabbb8ec907f9953e1d2b05a43b8451

--- device.mk
+++ device.mk
@@ -106,6 +106,9 @@ PRODUCT_PACKAGES += \
     libvolumelistener
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
+
+PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
