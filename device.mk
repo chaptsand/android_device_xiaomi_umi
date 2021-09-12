54edd6323e81def80727f35d8502710bce4b0025
Lokesh Gidra <lokeshgidra@google.com>
2021-09-12 23:59:07 +0800

    umi: Use 6GB dalvik-heap config
    Bug: 145823510
    Test: MPTS
    Change-Id: I086d4a5c09634c3c7bd51ec5d08dcaa1551511b4

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Init
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
