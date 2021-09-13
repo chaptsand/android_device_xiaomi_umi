b871337fc2a5df7124a9e1e4f4c1635870a94480
Bowgo Tsai <bowgotsai@google.com>
2021-09-13 19:19:05 +0800

    umi: Adding GSI keys
    Bug: 112293933
    Test: build and checks the keys are installed into ramdisk
    Change-Id: I216f3900e9511e0929418af050ff964898d0aea1

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Include GSI keys
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Init
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
