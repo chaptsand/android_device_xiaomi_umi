1cafc1f25f16842974e44488cf0f6954db791ad7
chaptsand <chaptsand@gmail.com>
2021-09-14 15:48:55 +0800

    umi: Import qti whitelist
     * from LA.UM.9.12.r1-12800-SMxx50.0
    
    Change-Id: I144258bb5e8f739cf8d525302f4db48401fb761d

--- device.mk
+++ device.mk
@@ -57,7 +57,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # QTI
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 29
