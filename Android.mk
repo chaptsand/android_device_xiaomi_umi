c35d691dda1e116a73b9dc01607f1b8c4a31397d
chaptsand <chaptsand@gmail.com>
2021-09-14 13:38:26 +0800

    umi: Create WiFi firmware symlinks
    Change-Id: I508ccb775624baac857c919c892e24e3dcd6ab6f

--- Android.mk
+++ Android.mk
@@ -73,10 +73,19 @@ $(RFS_MDM_SLPI_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	$(hide) ln -sf /vendor/firmware_mnt $@/readonly/firmware
 	$(hide) ln -sf /vendor/firmware $@/readonly/vendor/firmware
 
+WIFI_FIRMWARE_SYMLINKS := $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld
+$(WIFI_FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating WiFi firmware symlinks: $@"
+	@mkdir -p $@
+	$(hide) ln -sf /vendor/etc/wifi/qca6390/WCNSS_qcom_cfg.ini $@/WCNSS_qcom_cfg.ini
+	$(hide) ln -sf /mnt/vendor/persist/wlan_mac.bin $@/wlan_mac.bin
+
+
 ALL_DEFAULT_INSTALLED_MODULES += \
     $(RFS_MDM_ADSP_SYMLINKS) \
     $(RFS_MDM_CDSP_SYMLINKS) \
     $(RFS_MDM_MPSS_SYMLINKS) \
-    $(RFS_MDM_SLPI_SYMLINKS)
+    $(RFS_MDM_SLPI_SYMLINKS) \
+    $(WIFI_FIRMWARE_SYMLINKS)
 
 endif
