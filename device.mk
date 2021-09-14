9045165f559c6288bdc2a190af63cae88262cf28
Bruno Martins <bgcngm@gmail.com>
2021-09-14 16:05:20 +0800

    umi: List additional native libraries to expose to apps
    Change-Id: I552dc44a32221d2d3cec48e1c64ed72c470d412e

--- device.mk
+++ device.mk
@@ -13,6 +13,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Additional native libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # Fastboot
 PRODUCT_PACKAGES += \
     fastbootd
