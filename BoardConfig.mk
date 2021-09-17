4b365ba9ddaf91c4ba37db6770fce71efcb66c74
chaptsand <chaptsand@gmail.com>
2021-09-17 12:58:38 +0800

    umi: Add xiaomi specific VINTF entries
    Change-Id: Id5a527380568086f4bdda0b820a38cb68be9d17a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,10 +79,12 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)/device_framework_matrix.xml
+    $(DEVICE_PATH)/vintf/device_framework_matrix.xml \
+    $(DEVICE_PATH)/vintf/xiaomi_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
-    $(DEVICE_PATH)/vintf/manifest.xml
+    $(DEVICE_PATH)/vintf/manifest.xml \
+    $(DEVICE_PATH)/vintf/xiaomi_manifest.xml
 
 DEVICE_MATRIX_FILE := \
     $(DEVICE_PATH)/vintf/compatibility_matrix.xml
