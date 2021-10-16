77d0c2aa8325fb40e4ad482f9ae1837944f34120
Michael Bestas <mkbestas@lineageos.org>
2021-10-16 17:57:20 +0800

    umi: sepolicy: Switch to SYSTEM_EXT_{PUBLIC,PRIVATE}_SEPOLICY_DIRS
    * Fixes:
    warning: BOARD_PLAT_PRIVATE_SEPOLICY_DIR has been deprecated.
        Use SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS instead.
    warning: BOARD_PLAT_PUBLIC_SEPOLICY_DIR has been deprecated.
        Use SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS instead.
    Change-Id: I752602079de8ff4c5370fe3ec861b8746838d878

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -195,8 +195,8 @@ VENDOR_SECURITY_PATCH := 2021-08-01
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # WiFi
