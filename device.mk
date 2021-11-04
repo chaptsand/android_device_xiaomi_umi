3bc5172d77397412c3c2d5f03bde259da4db07cd
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2021-11-04 13:39:02 +0800

    umi: Build ParanoidDoze
    Change-Id: I15aa87442124d30d6f69eb8d2c10ce47d26475d5

--- device.mk
+++ device.mk
@@ -182,6 +182,13 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor
 
+# Doze
+PRODUCT_PACKAGES += \
+    ParanoidDoze
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.sensor.pickup=xiaomi.sensor.pickup
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
