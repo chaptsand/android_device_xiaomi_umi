fb8cd9af6e8643c83be95ba20513ebdf33a51557
TheScarastic <warabhishek@gmail.com>
2021-11-14 22:55:50 +0800

    umi: parts: Adapt to S style
     * Use Theme.SubSettingsBase for theme
     * Replace PreferenceActivity with CollapsingToolbarBaseActivity
     * Add Titles to prefernce screen
     * Remove onOptionsItemSelected and where neccessary move to activity
    
    Change-Id: If4de142724519aa095a2eb5196e77182461c247a

--- parts/src/org/lineageos/settings/doze/DozeSettingsActivity.java
+++ parts/src/org/lineageos/settings/doze/DozeSettingsActivity.java
@@ -18,9 +18,10 @@
 package org.lineageos.settings.doze;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.collapsingtoolbar.R;
 
-public class DozeSettingsActivity extends PreferenceActivity {
+public class DozeSettingsActivity extends CollapsingToolbarBaseActivity {
 
     private static final String TAG_DOZE = "doze";
 
@@ -28,7 +29,7 @@ public class DozeSettingsActivity extends PreferenceActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction().replace(android.R.id.content,
+        getFragmentManager().beginTransaction().replace(R.id.content_frame,
                 new DozeSettingsFragment(), TAG_DOZE).commit();
     }
 }
