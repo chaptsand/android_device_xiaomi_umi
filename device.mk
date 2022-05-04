239042338c42395d98cdba399a595dad74ef83de
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-05 00:24:47 +0200

    umi: Build mtdservice interface lib from source
    Change-Id: Ie739f283c4be1b77d57aaee1941ed46957da1d1a

--- device.mk
+++ device.mk
@@ -261,7 +261,8 @@ PRODUCT_COPY_FILES += \
 
 # Mlipay
 PRODUCT_PACKAGES += \
-    vendor.xiaomi.hardware.mlipay@1.1.vendor
+    vendor.xiaomi.hardware.mlipay@1.1.vendor \
+    vendor.xiaomi.hardware.mtdservice@1.0.vendor
 
 # NFC
 PRODUCT_PACKAGES += \
