6971d92c098b8ba6a9142b6d17773f893a2fd8b1
chaptsand <chaptsand@gmail.com>
2022-05-18 16:22:37 +0800

    umi: overlay: Convert overlay-lineage to AOSPA RRO packages
    Change-Id: I8b3b7ac8639c5b7dca1cff63028566f7f65a0250

--- device.mk
+++ device.mk
@@ -309,7 +309,6 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
@@ -320,6 +319,8 @@ PRODUCT_PACKAGES += \
     WifiResCommon_Sys
 
 PRODUCT_PACKAGES += \
+    AOSPAUmiSettings \
+    AOSPAUmiSystemUI \
     FrameworksResTarget \
     UmiFrameworks \
     UmiSystemUI \
