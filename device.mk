c009e2e503889c52eb647784717ed3bac1ca60ee
chaptsand <chaptsand@gmail.com>
2022-05-22 22:17:17 +0800

    umi: Remove duplicate fingerprint feature permission copy
    Change-Id: Ic8e5bda5a95b97542b4e88a2c8a23b8a2ba7a521

--- device.mk
+++ device.mk
@@ -114,9 +114,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
 # Biometrics
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
-
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.umi
 
