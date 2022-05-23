8e6c8e10e1f11edf17bc1593e2dd399307e30eb0
TH779 <i@779.moe>
2022-05-23 19:28:34 +0800

    umi: Don't call configureRpcThreadpool for camera postproc impl
     * In Android 12, no longer allowed to shrink the process pool after a
       larger process pool is started, we should patch camera postproc impl
       to nuke it from shrinking process pool and prevent triggering a fatal
       to cause the camera to crash.
       Ref: https://android.googlesource.com/platform/system/libhwbinder/+/8e047f7a626c782a02dc9e67dee2dbae35bc6956.
    
     * The following are the difference between before and after the
       patch.
       Before:
       MOV    W1, #1
       BL     ._ZN7android8hardware22configureRpcThreadpoolEmb
       Now:
       MOV    W1, #1
       NOP
    
     * Obtained by comparing the camera postproc impl modified by
       GitHub@erfanoabdi.
    
     * A tutorial on use hexdump, sed and xxd to patch hex in
       binary file: https://everydaywithlinux.blogspot.com/2012/11/patch-strings-in-binary-files-with-sed.html.
    
    Co-authored-by: Erfan Abdi <erfangplus@gmail.com>
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I66c257e80bf56a48c640efc3c1c8949852edf756

--- extract-files.sh
+++ extract-files.sh
@@ -55,6 +55,10 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+        vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
+            hexdump -ve '1/1 "%.2X"' "${2}" | sed "s/210080529A0A0094/210080521F2003D5/g" | xxd -r -p > "${TMPDIR}/${1##*/}"
+            mv "${TMPDIR}/${1##*/}" "${2}"
+            ;;
     esac
 }
 
