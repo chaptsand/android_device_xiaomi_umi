599acc4993aca720576de3e8620514df9fba0cc3
Alexander Koskovich <akoskovich@pm.me>
2022-06-09 22:53:08 +0800

    umi: Drop all component overrides
     * Qualcomm no longer disables these for targets
       that support RRO, e.g. all QSSI targets.
    
    Change-Id: I0372817623e8d0e58926fbfb519671c3caa07b86

--- device.mk
+++ device.mk
@@ -168,10 +168,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
-# Component overrides
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
