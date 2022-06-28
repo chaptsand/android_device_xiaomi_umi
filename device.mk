89819d8c423d25eb7e118a245065dadf72ffae9a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-06-28 16:49:46 +0800

    umi: Go away Megvii
    * Nuke Megvii proprietary files and build stub ones from hardware/xiaomi
    
    Change-Id: I53f8f0897b25517741a06c5962c02fe8fda61072

--- device.mk
+++ device.mk
@@ -154,6 +154,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
+PRODUCT_PACKAGES += \
+    libMegviiFacepp-0.5.2 \
+    libmegface
+
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
