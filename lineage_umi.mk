f77575f5aa7ff1f6c613bef1ed5f24b221ca28ab
chaptsand <chaptsand@gmail.com>
2022-08-23 08:08:47 +0000

    umi: Update blobs from UMI V13.0.7.0.SJBCNXM
    Change-Id: I90c7ac50b7bcf205ed2d11a0e50220333ea79d52

--- lineage_umi.mk
+++ lineage_umi.mk
@@ -22,4 +22,4 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Xiaomi/umi/umi:12/SKQ1.211006.001/V13.0.6.0.SJBMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/umi/umi:12/SKQ1.211006.001/V13.0.7.0.SJBCNXM:user/release-keys
