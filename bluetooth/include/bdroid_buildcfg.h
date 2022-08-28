e78f0da2cc80c6f96cb03dd9e73038a72a178d58
chaptsand <chaptsand@gmail.com>
2022-08-28 02:17:59 +0000

    umi: Switch to AOSP BT for now
    Change-Id: I7d09a3516bc5f50703c1d492e2598d1444011423

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -24,9 +24,8 @@
 
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
-#define MAX_L2CAP_CHANNELS    32
+#define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
-#define GATT_MAX_PHY_CHANNEL  10
-
-#define AVDT_NUM_SEPS 35
+// Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec
+#define AVDT_NUM_SEPS 12
 #endif
