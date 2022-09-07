41a45322c706cbdd75e1a056c8d87c37d0aa57de
Bowgo Tsai <bowgotsai@google.com>
2022-09-08 01:10:16 +0800

    umi: Removing gsi_keys.mk
    It's no longer required to install the compliance GSI
    public keys into the ramdisk. Those public keys just need
    to be included in the VTS test suite for GSI verification.
    
    Bug: 176869478
    Test: TreeHugger
    Change-Id: I26c4a1c7dba28a3c8cf88af64d8539fdc23a4f28

--- device.mk
+++ device.mk
@@ -7,9 +7,6 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# Include GSI keys
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
