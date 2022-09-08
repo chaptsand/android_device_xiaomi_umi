b4bbeca42e4df9c259d9699f0e564f3bc8197727
chaptsand <chaptsand@gmail.com>
2022-09-08 08:24:23 +0000

    umi: libperfmgr: Rebrand for xiaomi_umi
    Change-Id: Iaf645fb98ddf6fb7203cad038543515036a32095

--- power-libperfmgr/PowerExt.cpp
+++ power-libperfmgr/PowerExt.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power-service.pixel.ext-libperfmgr"
+#define LOG_TAG "android.hardware.power-service.xiaomi_umi.ext-libperfmgr"
 
 #include "PowerExt.h"
 
