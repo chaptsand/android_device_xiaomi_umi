f8b955a7ed590dd765cb7d4bd711aa9beb866bd6
chaptsand <chaptsand@gmail.com>
2022-10-08 21:27:21 +0800

    umi: Move away from Display split makefiles
    * Require all requested packages to exis
    
    Change-Id: Ibdf4a45b82e5a9f38c422c2ca97f4a701f21517b

--- device.mk
+++ device.mk
@@ -156,14 +156,28 @@ PRODUCT_PACKAGES += \
     disable_configstore
 
 # Display
-$(call inherit-product, hardware/qcom-caf/sm8250/display/config/display-product.mk)
-$(call inherit-product, vendor/qcom/opensource/commonsys-intf/display/config/display-interfaces-product.mk)
-$(call inherit-product, vendor/qcom/opensource/commonsys-intf/display/config/display-product-system.mk)
-$(call inherit-product, vendor/qcom/opensource/commonsys/display/config/display-product-commonsys.mk)
-$(call inherit-product, vendor/qcom/opensource/display/config/display-product-vendor.mk)
-
-BOARD_DISPLAY_HAL := hardware/qcom-caf/sm8250/display
-TARGET_BOARD_PLATFORM := kona
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
+    gralloc.kona \
+    libdisplayconfig.qti \
+    libdisplayconfig.system.qti \
+    libqdMetaData \
+    libqdMetaData.system \
+    libtinyxml \
+    memtrack.kona \
+    vendor.display.config@1.0 \
+    vendor.display.config@1.11.vendor \
+    vendor.display.config@2.0 \
+    vendor.display.config@2.0.vendor \
+    vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.display.composer-service \
+    vendor.qti.hardware.display.mapper@1.1.vendor \
+    vendor.qti.hardware.display.mapper@2.0.vendor \
+    vendor.qti.hardware.display.mapper@3.0.vendor \
+    vendor.qti.hardware.display.mapper@4.0.vendor
 
 # DRM
 PRODUCT_PACKAGES += \
