4a43580b87f50418cf04e060df564ff682dc02f3
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-01-15 17:04:09 +0800

    umi: : Move to AOSP NXP NFC HAL
    * NXP added snxxx support in AOSP, \o/
    
    Change-Id: Id1b9cdbe1f29d1ff61af8182ae2f8370e5210426
    Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

--- device.mk
+++ device.mk
@@ -288,7 +288,7 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service \
+    android.hardware.nfc_snxxx@1.2-service \
     android.hardware.secure_element@1.2.vendor \
     com.android.nfc_extras \
     libchrome.vendor \
